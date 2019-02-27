5350ee849af611057e2ebca75b963af260e36954
kimhyunjin <khjzzm@gmail.com>
2019-02-27 11:37:54 +0900

    added SendATS buttons param

--- KakaoExample/Controllers/KakaoController.cs
+++ KakaoExample/Controllers/KakaoController.cs
@@ -144,10 +144,10 @@ namespace KakaoExample.Controllers
         public IActionResult SendATS_One()
         {
             // 알림톡 템플릿 코드, ListATSTemplate API의 templateCode 확인
-            string templateCode = "018110000047";
+            string templateCode = "019020000163";
 
             // 발신번호
-            string senderNum = "07043042992";
+            string senderNum = "07043042991";
 
             // 수신번호
             string receiverNum = "010111222";
@@ -155,8 +155,13 @@ namespace KakaoExample.Controllers
             // 수신자명
             string receiverName = "수신자명";
 
-            // 알림톡 템플릿 내용 (최대 1000자)
-            string content = "[테스트] 테스트 템플릿입니다.";
+            // 알림톡 템플릿 내용, 최대 1000자
+            String content = "[ 팝빌 ]\n";
+            content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n";
+            content += "해당 템플릿으로 전송 가능합니다.\n\n";
+            content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n";
+            content += "팝빌 파트너센터 : 1600-8536\n";
+            content += "support@linkhub.co.kr".Replace("\n", Environment.NewLine);
 
             // 대체문자 메시지 내용 (최대 2000byte)
             string altContent = "대체문자 메시지 내용";
@@ -172,10 +177,30 @@ namespace KakaoExample.Controllers
             // 최대 36자리, 영문, 숫자, 언더바('_'), 하이픈('-')을 조합하여 사업자별로 중복되지 않도록 구성
             string requestNum = "";
 
+            // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 null 처리.
+            List<KakaoButton> buttons = null;
+
+
+            // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+            /*
+            List<KakaoButton> buttons = new List<KakaoButton>();
+            
+            KakaoButton btnInfo = new KakaoButton();
+            // 버튼명
+            btnInfo.n = "템플릿 안내";
+            // 버튼유형 DS(-배송조회 / WL - 웹링크 / AL - 앱링크 / MD - 메시지전달 / BK - 봇키워드)
+            btnInfo.t = "WL";
+            // 버튼링크1 [앱링크] Android / [웹링크] Mobile
+            btnInfo.u1 = "https://www.popbill.com";
+            // 버튼링크2 [앱링크] IOS / [웹링크] PC URL
+            btnInfo.u2 = "http://test.popbill.com";
+            buttons.Add(btnInfo);
+            */
+
             try
             {
                 var receiptNum = _kakaoService.SendATS(corpNum, templateCode, senderNum, receiverNum, receiverName,
-                    content, altContent, altSendType, sndDT, requestNum);
+                    content, altContent, altSendType, sndDT, requestNum, userID, buttons);
                 return View("ReceiptNum", receiptNum);
             }
             catch (PopbillException pe)
@@ -188,10 +213,18 @@ namespace KakaoExample.Controllers
          * [대량전송] 알림톡 전송을 요청합니다.
          * 사전에 승인된 템플릿의 내용과 알림톡 전송내용(content)이 다를 경우 전송실패 처리됩니다.
          */
-        public IActionResult SendATS_Multi()
+            public IActionResult SendATS_Multi()
         {
             // 알림톡 템플릿 코드, ListATSTemplate API의 templateCode 확인
-            string templateCode = "018110000047";
+            string templateCode = "019020000163";
+
+            // 알림톡 템플릿 내용, 최대 1000자
+            String content = "[ 팝빌 ]\n";
+            content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n";
+            content += "해당 템플릿으로 전송 가능합니다.\n\n";
+            content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n";
+            content += "팝빌 파트너센터 : 1600-8536\n";
+            content += "support@linkhub.co.kr".Replace("\n", Environment.NewLine);
 
             // 발신번호
             string senderNum = "07043042992";
@@ -209,7 +242,7 @@ namespace KakaoExample.Controllers
                 receiverInfo.rcvnm = "수신자명" + i;
 
                 // 알림톡 템플릿 내용 (최대 1000자)
-                receiverInfo.msg = "[테스트] 테스트 템플릿입니다." + i;
+                receiverInfo.msg = content;
 
                 // 대체문자 내용 (최대 2000byte)
                 receiverInfo.altmsg = "대체문자 내용입니다" + i;
@@ -228,10 +261,30 @@ namespace KakaoExample.Controllers
             // 최대 36자리, 영문, 숫자, 언더바('_'), 하이픈('-')을 조합하여 사업자별로 중복되지 않도록 구성
             string requestNum = "";
 
+            // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 null 처리.
+            List<KakaoButton> buttons = null;
+
+
+            // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+            /*
+            List<KakaoButton> buttons = new List<KakaoButton>();
+            
+            KakaoButton btnInfo = new KakaoButton();
+            // 버튼명
+            btnInfo.n = "템플릿 안내";
+            // 버튼유형 DS(-배송조회 / WL - 웹링크 / AL - 앱링크 / MD - 메시지전달 / BK - 봇키워드)
+            btnInfo.t = "WL";
+            // 버튼링크1 [앱링크] Android / [웹링크] Mobile
+            btnInfo.u1 = "https://www.popbill.com";
+            // 버튼링크2 [앱링크] IOS / [웹링크] PC URL
+            btnInfo.u2 = "http://test.popbill.com";
+            buttons.Add(btnInfo);
+            */
+
             try
             {
                 var receiptNum = _kakaoService.SendATS(corpNum, templateCode, senderNum, receivers, altSendType, sndDT,
-                    requestNum);
+                    requestNum, userID, buttons);
                 return View("ReceiptNum", receiptNum);
             }
             catch (PopbillException pe)
@@ -247,17 +300,22 @@ namespace KakaoExample.Controllers
         public IActionResult SendATS_Same()
         {
             // 알림톡 템플릿 코드, ListATSTemplate API의 templateCode 확인
-            string templateCode = "018110000047";
+            string templateCode = "019020000163";
 
-            // 발신번호
-            string senderNum = "07043042992";
-
-            // (동보) 알림톡 템플릿 내용 (최대 1000자)
-            string content = "[테스트] 테스트 템플릿입니다.";
+            // 알림톡 템플릿 내용, 최대 1000자
+            String content = "[ 팝빌 ]\n";
+            content += "신청하신 #{템플릿코드}에 대한 심사가 완료되어 승인 처리되었습니다.\n";
+            content += "해당 템플릿으로 전송 가능합니다.\n\n";
+            content += "문의사항 있으시면 파트너센터로 편하게 연락주시기 바랍니다.\n\n";
+            content += "팝빌 파트너센터 : 1600-8536\n";
+            content += "support@linkhub.co.kr".Replace("\n", Environment.NewLine);
 
             // (동보) 대체문자 메시지 내용 (최대 2000byte)
             string altContent = "대체문자 메시지 내용";
 
+            // 발신번호
+            string senderNum = "07043042991";
+            
             // 수신자정보 배열, 최대 1000건
             List<KakaoReceiver> receivers = new List<KakaoReceiver>();
             for (int i = 0; i < 5; i++)
@@ -284,10 +342,30 @@ namespace KakaoExample.Controllers
             // 최대 36자리, 영문, 숫자, 언더바('_'), 하이픈('-')을 조합하여 사업자별로 중복되지 않도록 구성
             string requestNum = "";
 
+            // 알림톡 버튼정보를 템플릿 신청시 기재한 버튼정보와 동일하게 전송하는 경우 null 처리.
+            List<KakaoButton> buttons = null;
+
+
+            // 알림톡 버튼 URL에 #{템플릿변수}를 기재한경우 템플릿변수 영역을 변경하여 버튼정보 구성
+            /*
+            List<KakaoButton> buttons = new List<KakaoButton>();
+            
+            KakaoButton btnInfo = new KakaoButton();
+            // 버튼명
+            btnInfo.n = "템플릿 안내";
+            // 버튼유형 DS(-배송조회 / WL - 웹링크 / AL - 앱링크 / MD - 메시지전달 / BK - 봇키워드)
+            btnInfo.t = "WL";
+            // 버튼링크1 [앱링크] Android / [웹링크] Mobile
+            btnInfo.u1 = "https://www.popbill.com";
+            // 버튼링크2 [앱링크] IOS / [웹링크] PC URL
+            btnInfo.u2 = "http://test.popbill.com";
+            buttons.Add(btnInfo);
+            */
+
             try
             {
                 var receiptNum = _kakaoService.SendATS(corpNum, templateCode, senderNum, content, altContent, receivers,
-                    altSendType, sndDT, requestNum);
+                    altSendType, sndDT, requestNum, userID, buttons);
                 return View("ReceiptNum", receiptNum);
             }
             catch (PopbillException pe)
