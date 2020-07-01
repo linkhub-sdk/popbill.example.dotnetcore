c3c94bc31bdc7f8ed90dc52f8e812a0e1ce79c5e
JohnJeong123 <frenchofkiss@gmail.com>
2020-07-01 13:45:41 +0900

    added AccountCheckService API Example

---
+++ AccountCheckExample/Controllers/AccountCheckController.cs
@@ -0,0 +1,429 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Popbill;
+using Popbill.AccountCheck;
+
+namespace AccountCheckExample.Controllers
+{
+    public class AccountCheckController : Controller
+    {
+        private readonly AccountCheckService _accountCheckService;
+
+        public AccountCheckController(AccountCheckInstance ACInstance)
+        {
+            //예금주조회 서비스 객체 생성
+            _accountCheckService = ACInstance.accountCheckService;
+
+        }
+
+        //팝빌 연동회원 사업자번호 (하이픈 '-' 없이 10자리)
+        string corpNum = "1234567890";
+
+        //팝빌 연동회원 아이디
+        string userID = "testkorea";
+
+        /*
+         * 예금주조회 Index page (Closedown/Index.cshtml)
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        
+        /*
+         * 1건의 계좌에 대한 예금주정보 조회합니다.
+         */
+        public IActionResult CheckAccountInfo()
+        {
+            try
+            {
+                // 기관코드
+                string bankCode = "0004";
+
+                // 계좌번호
+                string accountNumber = "94324511758";
+
+
+                var response = _accountCheckService.CheckAccountInfo(corpNum, bankCode, accountNumber);
+                return View("CheckAccountInfo", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원 잔여포인트를 확인합니다.
+         */
+        public IActionResult GetBalance()
+        {
+            try
+            {
+                var result = _accountCheckService.GetBalance(corpNum);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 팝빌 연동회원의 포인트충전 팝업 URL을 반환합니다.
+         * - 반환된 URL은 보안정책에 따라 30초의 유효시간을 갖습니다.
+         */
+        public IActionResult GetChargeURL()
+        {
+            try
+            {
+                var result = _accountCheckService.GetChargeURL(corpNum, userID);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 파트너의 잔여포인트를 확인합니다.
+         * - 과금방식이 연동과금인 경우 연동회원 잔여포인트(GetBalance API)를 이용하시기 바랍니다.
+         */
+        public IActionResult GetPartnerBalance()
+        {
+            try
+            {
+                var result = _accountCheckService.GetPartnerBalance(corpNum);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 파트너 포인트 충전 팝업 URL을 반환합니다.
+         * - 반환된 URL은 보안정책에 따라 30초의 유효시간을 갖습니다.
+         */
+        public IActionResult GetPartnerURL()
+        {
+            // CHRG - 포인트충전 URL
+            string TOGO = "CHRG";
+
+            try
+            {
+                var result = _accountCheckService.GetPartnerURL(corpNum, TOGO);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 예금주조회 조회단가를 확인합니다.
+         */
+        public IActionResult GetUnitCost()
+        {
+            try
+            {
+                var result = _accountCheckService.GetUnitCost(corpNum);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 예금주조회 API 서비스 과금정보를 확인합니다.
+         */
+        public IActionResult GetChargeInfo()
+        {
+            try
+            {
+                var response = _accountCheckService.GetChargeInfo(corpNum);
+                return View("GetChargeInfo", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 해당 사업자의 파트너 연동회원 가입여부를 확인합니다.
+         */
+        public IActionResult CheckIsMember()
+        {
+            try
+            {
+                //링크아이디
+                string linkID = "TESTER";
+
+                var response = _accountCheckService.CheckIsMember(corpNum, linkID);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 팝빌 회원아이디 중복여부를 확인합니다.
+         */
+        public IActionResult CheckID()
+        {
+            //중복여부 확인할 팝빌 회원 아이디
+            string checkID = "testkorea";
+
+            try
+            {
+                var response = _accountCheckService.CheckID(checkID);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 파트너의 연동회원으로 신규가입 처리합니다.
+         */
+        public IActionResult JoinMember()
+        {
+            JoinForm joinInfo = new JoinForm();
+
+            // 링크아이디
+            joinInfo.LinkID = "TESTER";
+
+            // 아이디, 6자이상 50자 미만
+            joinInfo.ID = "userid_20181212";
+
+            // 비밀번호, 6자이상 20자 미만
+            joinInfo.PWD = "12341234";
+
+            // 사업자번호 "-" 제외
+            joinInfo.CorpNum = "0000000001";
+
+            // 대표자 성명 (최대 100자)
+            joinInfo.CEOName = "대표자 성명";
+
+            // 상호 (최대 200자)
+            joinInfo.CorpName = "상호";
+
+            // 주소 (최대 300자)
+            joinInfo.Addr = "주소";
+
+            // 업태 (최대 100자)
+            joinInfo.BizType = "업태";
+
+            // 종목 (최대 100자)
+            joinInfo.BizClass = "종목";
+
+            // 담당자 성명 (최대 100자)
+            joinInfo.ContactName = "담당자명";
+
+            // 담당자 이메일주소 (최대 100자)
+            joinInfo.ContactEmail = "test@test.com";
+
+            // 담당자 연락처 (최대 20자)
+            joinInfo.ContactTEL = "070-4304-2992";
+
+            // 담당자 휴대폰번호 (최대 20자)
+            joinInfo.ContactHP = "010-111-222";
+
+            // 담당자 팩스번호 (최대 20자)
+            joinInfo.ContactFAX = "02-111-222";
+
+            try
+            {
+                var response = _accountCheckService.JoinMember(joinInfo);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원의 회사정보를 확인합니다.
+         */
+        public IActionResult GetCorpInfo()
+        {
+            try
+            {
+                var response = _accountCheckService.GetCorpInfo(corpNum, userID);
+                return View("GetCorpInfo", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원의 회사정보를 수정합니다
+         */
+        public IActionResult UpdateCorpInfo()
+        {
+            CorpInfo corpInfo = new CorpInfo();
+
+            // 대표자 성명 (최대 100자)
+            corpInfo.ceoname = "대표자 성명 수정";
+
+            // 상호 (최대 200자)
+            corpInfo.corpName = "상호 수정";
+
+            // 주소 (최대 300자)
+            corpInfo.addr = "주소 수정";
+
+            // 업태 (최대 100자)
+            corpInfo.bizType = "업태 수정";
+
+            // 종목 (최대 100자)
+            corpInfo.bizClass = "종목 수정";
+
+            try
+            {
+                var response = _accountCheckService.UpdateCorpInfo(corpNum, corpInfo, userID);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원의 담당자를 신규로 등록합니다.
+         */
+        public IActionResult RegistContact()
+        {
+            Contact contactInfo = new Contact();
+
+            // 담당자 아이디, 6자 이상 50자 미만
+            contactInfo.id = "testkorea_20181212";
+
+            // 비밀번호, 6자 이상 20자 미만
+            contactInfo.pwd = "user_password";
+
+            // 담당자명 (최대 100자)
+            contactInfo.personName = "코어담당자";
+
+            // 담당자 연락처 (최대 20자)
+            contactInfo.tel = "070-4304-2992";
+
+            // 담당자 휴대폰번호 (최대 20자)
+            contactInfo.hp = "010-111-222";
+
+            // 담당자 팩스번호 (최대 20자)
+            contactInfo.fax = "02-111-222";
+
+            // 담당자 이메일 (최대 100자)
+            contactInfo.email = "netcore@linkhub.co.kr";
+
+            // 회사조회 권한여부, true(회사조회), false(개인조회)
+            contactInfo.searchAllAllowYN = true;
+
+            // 관리자 권한여부, true(관리자), false(사용자)
+            contactInfo.mgrYN = false;
+
+            try
+            {
+                var response = _accountCheckService.RegistContact(corpNum, contactInfo, userID);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원의 담당자 목록을 확인합니다.
+         */
+        public IActionResult ListContact()
+        {
+            try
+            {
+                var response = _accountCheckService.ListContact(corpNum, userID);
+                return View("ListContact", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 연동회원의 담당자 정보를 수정합니다.
+         */
+        public IActionResult UpdateContact()
+        {
+            Contact contactInfo = new Contact();
+
+            // 담당자 아이디
+            contactInfo.id = "testkorea";
+
+            // 담당자명 (최대 100자)
+            contactInfo.personName = "코어담당자";
+
+            // 담당자 연락처 (최대 20자)
+            contactInfo.tel = "070-4304-2992";
+
+            // 담당자 휴대폰번호 (최대 20자)
+            contactInfo.hp = "010-111-222";
+
+            // 담당자 팩스번호 (최대 20자)
+            contactInfo.fax = "02-111-222";
+
+            // 담당자 이메일 (최대 10자)
+            contactInfo.email = "netcore@linkhub.co.kr";
+
+            // 회사조회 권한여부, true(회사조회), false(개인조회)
+            contactInfo.searchAllAllowYN = true;
+
+            // 관리자 권한여부, true(관리자), false(사용자)
+            contactInfo.mgrYN = false;
+
+            try
+            {
+                var response = _accountCheckService.UpdateContact(corpNum, contactInfo, userID);
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+        /*
+         * 팝빌에 로그인 상태로 접근할 수 있는 팝업 URL을 반환합니다.
+         * - 반환된 URL은 보안정책에 따라 30초의 유효시간을 갖습니다.
+         */
+        public IActionResult GetAccessURL()
+        {
+            try
+            {
+                var result = _accountCheckService.GetAccessURL(corpNum, userID);
+                return View("Result", result);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
+
+    }
+}
---
+++ AccountCheckExample/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Popbill.AccountCheck;
+
+public class AccountCheckInstance
+{
+    //파트너 신청 후 메일로 발급받은 링크아이디(LinkID)와 비밀키(SecretKey)값 으로 변경하시기 바랍니다.
+    private string linkID = "TESTER";
+    private string secretKey = "SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=";
+
+    public AccountCheckService accountCheckService;
+
+    public AccountCheckInstance()
+    {
+        //예금주조회 서비스 객체 초기화
+        accountCheckService = new AccountCheckService(linkID, secretKey);
+
+        //연동환경 설정값, 개발용(true), 상업용(false)
+        accountCheckService.IsTest = true;
+
+        //인증토큰의 IP제한기능 사용여부, 권장(true)
+        accountCheckService.IPRestrictOnOff = true;
+    }
+}
+
+
+namespace AccountCheckExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //예금주조회 서비스 객체 의존성 주입
+            services.AddSingleton<AccountCheckInstance>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=AccountCheck}/{action=Index}");
+            });
+        }
+    }
+}
