a8127bf5bbfc90df60f7bcec71c7f90d01c8d1b1
1Jae <wjkim@linkhub.co.kr>
2021-05-18 13:33:00 +0900

    added DeleteBankAccount

--- EasyFinBankExample/Controllers/EasyFinBankController.cs
+++ EasyFinBankExample/Controllers/EasyFinBankController.cs
@@ -221,6 +221,31 @@ namespace EasyFinBankExample.Controllers
             }
         }
 
+        public IActionResult DeleteBankAccount()
+        {
+            EasyFinBankAccountForm info = new EasyFinBankAccountForm();
+
+            // [필수] 은행코드
+            // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
+            // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
+            // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
+            info.BankCode = "";
+
+            // [필수] 계좌번호, 하이픈('-') 제외
+            info.AccountNumber = "";
+
+            try
+            {
+                var response = _easyFinBankService.DeleBankAccount(corpNum, info, userID);
+
+                return View("Response", response);
+            }
+            catch (PopbillException pe)
+            {
+                return View("Exception", pe);
+            }
+        }
+
 
         /*
          * 팝빌 계좌 관리 팝업 URL을 반환합니다.
