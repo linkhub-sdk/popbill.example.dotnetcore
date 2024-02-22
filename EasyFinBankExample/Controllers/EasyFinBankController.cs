e62272a10e83a521bd9065a00b2918fe7705d4b3
박지헌 <jhpark@linkhubcorp.com>
2024-02-22 18:02:38 +0900

    UpdateBankAccount 업데이트

--- EasyFinBankExample/Controllers/EasyFinBankController.cs
+++ EasyFinBankExample/Controllers/EasyFinBankController.cs
@@ -95,16 +95,16 @@ namespace EasyFinBankExample.Controllers
          */
         public IActionResult UpdateBankAccount()
         {
-            EasyFinBankAccountForm info = new EasyFinBankAccountForm();
+            UpdateEasyFinBankAccountForm info = new UpdateEasyFinBankAccountForm();
 
             // 기관코드
             // 산업은행-0002 / 기업은행-0003 / 국민은행-0004 /수협은행-0007 / 농협은행-0011 / 우리은행-0020
             // SC은행-0023 / 대구은행-0031 / 부산은행-0032 / 광주은행-0034 / 제주은행-0035 / 전북은행-0037
             // 경남은행-0039 / 새마을금고-0045 / 신협은행-0048 / 우체국-0071 / KEB하나은행-0081 / 신한은행-0088 /씨티은행-0027
-            info.BankCode = "";
+            string BankCode = "";
 
             // 계좌번호, 하이픈('-') 제외
-            info.AccountNumber = "";
+            string AccountNumber = "";
 
             // 계좌비밀번호
             info.AccountPWD = "";
@@ -127,7 +127,7 @@ namespace EasyFinBankExample.Controllers
 
             try
             {
-                var response = _easyFinBankService.UpdateBankAccount(corpNum, info);
+                var response = _easyFinBankService.UpdateBankAccount(corpNum, BankCode, AccountNumber, info);
 
                 return View("Response", response);
             }
