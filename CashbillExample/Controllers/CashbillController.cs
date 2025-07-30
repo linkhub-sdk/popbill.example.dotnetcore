3a756926c32414e380eae76260ab243a8cad953d
JohnJeong123 <frenchofkiss@gmail.com>
2025-07-30 16:34:40 +0900

    remove EmailType CSH_CANCEL

--- CashbillExample/Controllers/CashbillController.cs
+++ CashbillExample/Controllers/CashbillController.cs
@@ -867,7 +867,6 @@ namespace CashbillExample.Controllers
         {
             //메일전송유형
             // CSH_ISSUE : 고객에게 현금영수증이 발행 되었음을 알려주는 메일 입니다.
-            // CSH_CANCEL : 고객에게 현금영수증이 발행취소 되었음을 알려주는 메일 입니다.
             string emailType = "CSH_ISSUE";
 
             //전송여부 (true-전송, false-미전송)
